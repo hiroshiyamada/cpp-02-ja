743b62b17a1b0473e5178ace32b3b5477b659c59
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2024-03-10 18:39:40 +0900

    Add m3-u2-c3 codes

--- 01_objects/03_challenge/bank_account.cpp
+++ 01_objects/03_challenge/bank_account.cpp
@@ -1,1 +1,44 @@
-// コードを入力してください
+#include "bank_account.h"
+#include <iostream>
+
+BankAccount::BankAccount(double balance) : balance(balance)
+{
+    print_current_money();
+}
+
+void BankAccount::deposit(double num)
+{
+    if (num < 0)
+    {
+        std::cout << "Please insert valid money." << std::endl;
+        return;
+    }
+    balance += num;
+    print_current_money();
+}
+
+void BankAccount::withdraw(double num)
+{
+    if (num < 0)
+    {
+        std::cout << "Please insert valid money." << std::endl;
+        return;
+    }
+    if ((balance - num) < 0)
+    {
+        std::cout << "Insufficient balance!" << std::endl;
+        return;
+    }
+    balance -= num;
+    print_current_money();
+}
+
+double BankAccount::get_balance() const
+{
+    return balance;
+}
+
+void BankAccount::print_current_money() const
+{
+    std::cout << "Current money is " << balance << " yen." << std::endl;
+}
--- 01_objects/03_challenge/bank_account.h
+++ 01_objects/03_challenge/bank_account.h
@@ -1,1 +1,12 @@
-// コードを入力してください
+class BankAccount
+{
+public:
+    BankAccount(double balance);
+    void deposit(double num);
+    void withdraw(double num);
+    double get_balance() const;
+
+private:
+    double balance;
+    void print_current_money() const;
+};
