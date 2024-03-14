a821c58d5616b544fd4d1b79fd8ef20875f3d424
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2024-03-14 22:31:42 +0900

    Add u3-m3-c2 codes

--- 03_containers_and_templates/02_challenge/main.cpp
+++ 03_containers_and_templates/02_challenge/main.cpp
@@ -1,1 +1,14 @@
-// コードを入力してください
+#include "stack.hpp"
+
+int main(){
+    Stack<int> intStack;
+    intStack.push(2);
+    intStack.push(3);
+    std::cout << intStack.top() << std::endl;
+    intStack.pop();
+    std::cout << intStack.top() << std::endl;
+    std::cout << intStack.is_empty() << std::endl;
+    intStack.pop();
+    std::cout << intStack.is_empty() << std::endl;
+    return 0;
+}
--- 03_containers_and_templates/02_challenge/stack.hpp
+++ 03_containers_and_templates/02_challenge/stack.hpp
@@ -1,1 +1,47 @@
-// コードを入力してください
+#include <iostream>
+#include <vector>
+
+template <typename T>
+class Stack
+{
+public:
+    void push(const T &value)
+    {
+        data.push_back(value);
+    }
+
+    T pop()
+    {
+        if (!data.empty())
+        {
+            T value = data.back();
+            data.pop_back();
+            return value;
+        }
+        else
+        {
+            std::cerr << "Error: Stack is empty.\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    T top(){
+        if (!data.empty())
+        {
+            return data.back();
+        }
+        else
+        {
+            std::cerr << "Error: Stack is empty.\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    bool is_empty() const
+    {
+        return data.empty();
+    }
+
+private:
+    std::vector<T> data;
+};
