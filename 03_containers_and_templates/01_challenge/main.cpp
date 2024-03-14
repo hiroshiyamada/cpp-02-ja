c22e57f9037ace4a9dadddfb93069212726af9ed
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2024-03-14 22:13:38 +0900

    Add u3-m3-c1 codes

---
+++ 03_containers_and_templates/01_challenge/main.cpp
@@ -0,0 +1,10 @@
+#include "print.hpp"
+
+#include <vector>
+
+int main()
+{
+    std::vector<int> vec{1, 2, 3};
+    print_elements(vec);
+    return 0;
+}
--- 03_containers_and_templates/01_challenge/print.hpp
+++ 03_containers_and_templates/01_challenge/print.hpp
@@ -1,1 +1,10 @@
-// コードを入力してください
+#include <iostream>
+
+template <typename T>
+void print_elements(T elements)
+{
+    for (auto &element : elements)
+    {
+        std::cout << element << std::endl;
+    }
+}
