54ac5e608a9442bfdc291bf66cc4278e2cfd1612
Hiroshi Yamada <hiroshi.yamada426@gmail.com>
2024-03-14 22:34:24 +0900

    add test case

--- 03_containers_and_templates/01_challenge/main.cpp
+++ 03_containers_and_templates/01_challenge/main.cpp
@@ -1,10 +1,13 @@
 #include "print.hpp"
 
 #include <vector>
+#include <list>
 
 int main()
 {
     std::vector<int> vec{1, 2, 3};
     print_elements(vec);
+    std::list<float> float_list{1.5, 2.3, 3.2};
+    print_elements(float_list);
     return 0;
 }
